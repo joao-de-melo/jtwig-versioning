57a497177c7b55bee4ba47077df6005b10a84e40
Joao De Melo <jmelo@lyncode.com>
2017-02-25 00:27:11 +0000

    Handling new versions

--- src/main/java/org/jtwig/version/NextVersionFinder.java
+++ src/main/java/org/jtwig/version/NextVersionFinder.java
@@ -4,11 +4,17 @@ public class NextVersionFinder {
     public String nextVersion (String currentVersion) {
         if (currentVersion == null) {
             return "1.0";
+        } else if (currentVersion.endsWith(".RELEASE")) {
+            return getVersion(currentVersion.substring(0, currentVersion.lastIndexOf("."))) + ".RELEASE";
         } else {
-            int indexOfDot = currentVersion.lastIndexOf(".") + 1;
-            String prefix = currentVersion.substring(0, indexOfDot);
-            int number = Integer.valueOf(currentVersion.substring(indexOfDot));
-            return prefix + (number + 1);
+            return getVersion(currentVersion);
         }
+    }
+
+    private String getVersion(String currentVersion) {
+        int indexOfDot = currentVersion.lastIndexOf(".") + 1;
+        String prefix = currentVersion.substring(0, indexOfDot);
+        int number = Integer.valueOf(currentVersion.substring(indexOfDot));
+        return prefix + (number + 1);
     }
 }
